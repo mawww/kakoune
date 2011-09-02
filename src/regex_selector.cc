61b37a6057db4f6474029f9c94534f3a1a016b58
Maxime Coste <frrrwww@gmail.com>
2011-09-02 18:38:10 +0000

    RegexSelector: search the entire buffer instead of the current line

--- src/regex_selector.cc
+++ src/regex_selector.cc
@@ -14,12 +14,9 @@ Selection RegexSelector::operator()(const BufferIterator& cursor) const
 
     try
     {
-        while (not line_end.is_end() and *line_end != '\n')
-            ++line_end;
-
         boost::match_results<BufferIterator> matches;
 
-        if (boost::regex_search(cursor, line_end, matches, m_regex))
+        if (boost::regex_search(cursor, cursor.buffer().end(), matches, m_regex, boost::match_nosubs))
             return Selection(matches.begin()->first, matches.begin()->second);
     }
     catch (boost::regex_error& err)
