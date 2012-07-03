d092afead9ebd97c2274719730c6f2b5246a6087
Maxime Coste <frrrwww@gmail.com>
2012-07-03 21:12:05 +0200

    make DisplayBuffer::atom_containing easier to understand

--- src/display_buffer.cc
+++ src/display_buffer.cc
@@ -110,12 +110,16 @@ DisplayBuffer::iterator DisplayBuffer::atom_containing(const BufferIterator& whe
 DisplayBuffer::iterator DisplayBuffer::atom_containing(const BufferIterator& where,
                                                        iterator start)
 {
-    if (where < start->begin())
+    if (start == end() or where < start->begin())
         return end();
 
-    return std::upper_bound(start, end(), where,
-                            [](const BufferIterator& where, const DisplayAtom& atom)
-                            { return where < atom.end(); });
+    while (start != end())
+    {
+        if (start->end() > where)
+            break;
+        ++start;
+    }
+    return start;
 }
 
 DisplayBuffer::iterator DisplayBuffer::split(iterator atom, const BufferIterator& pos)
