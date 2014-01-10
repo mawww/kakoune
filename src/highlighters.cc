1d51e6922a262ce3531cd6dc313e50e9abd4fb77
Maxime Coste <frrrwww@gmail.com>
2014-01-10 13:36:40 +0000

    minor code cleanup

--- src/highlighters.cc
+++ src/highlighters.cc
@@ -250,7 +250,7 @@ HighlighterAndId colorize_regex_factory(HighlighterParameters params)
 
         String id = "colre'" + params[0] + "'";
 
-        Regex ex{params[0].begin(), params[0].end(), boost::regex::optimize};
+        Regex ex{params[0].begin(), params[0].end(), Regex::optimize};
 
         return HighlighterAndId(id, RegexColorizer(std::move(ex),
                                                    std::move(colors)));
@@ -635,8 +635,8 @@ HighlighterAndId region_factory(HighlighterParameters params)
         if (params.size() != 3)
             throw runtime_error("wrong parameter count");
 
-        Regex begin{params[0], boost::regex::nosubs | boost::regex::optimize };
-        Regex end{params[1], boost::regex::nosubs | boost::regex::optimize };
+        Regex begin{params[0], Regex::nosubs | Regex::optimize };
+        Regex end{params[1], Regex::nosubs | Regex::optimize };
         const ColorPair colors = get_color(params[2]);
 
         auto func = [colors](const Context&, DisplayBuffer& display_buffer,
@@ -662,8 +662,8 @@ HighlighterAndId region_ref_factory(HighlighterParameters params)
         if (params.size() != 3 and params.size() != 4)
             throw runtime_error("wrong parameter count");
 
-        Regex begin{params[0], boost::regex::nosubs | boost::regex::optimize };
-        Regex end{params[1], boost::regex::nosubs | boost::regex::optimize };
+        Regex begin{params[0], Regex::nosubs | Regex::optimize };
+        Regex end{params[1], Regex::nosubs | Regex::optimize };
         const String& name = params[2];
 
         auto func = [name](const Context& context, DisplayBuffer& display_buffer,
