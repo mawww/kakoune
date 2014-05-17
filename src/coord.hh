9240cccf74ddf175eb0dac5005633d19e3068539
Maxime Coste <frrrwww@gmail.com>
2014-05-17 10:17:28 +0100

    set Coords/Counts methods as always_inline
    They are well tested, and we never want to step into them when debugging

--- src/coord.hh
+++ src/coord.hh
@@ -12,14 +12,17 @@ struct LineAndColumn
     LineType   line;
     ColumnType column;
 
+    [[gnu::always_inline]]
     constexpr LineAndColumn(LineType line = 0, ColumnType column = 0)
         : line(line), column(column) {}
 
+    [[gnu::always_inline]]
     constexpr EffectiveType operator+(EffectiveType other) const
     {
         return EffectiveType(line + other.line, column + other.column);
     }
 
+    [[gnu::always_inline]]
     EffectiveType& operator+=(EffectiveType other)
     {
         line   += other.line;
@@ -27,11 +30,13 @@ struct LineAndColumn
         return *static_cast<EffectiveType*>(this);
     }
 
+    [[gnu::always_inline]]
     constexpr EffectiveType operator-(EffectiveType other) const
     {
         return EffectiveType(line - other.line, column - other.column);
     }
 
+    [[gnu::always_inline]]
     EffectiveType& operator-=(EffectiveType other)
     {
         line   -= other.line;
@@ -39,35 +44,41 @@ struct LineAndColumn
         return *static_cast<EffectiveType*>(this);
     }
 
+    [[gnu::always_inline]]
     constexpr bool operator< (EffectiveType other) const
     {
         return (line != other.line) ? line < other.line
                                     : column < other.column;
     }
 
+    [[gnu::always_inline]]
     constexpr bool operator<= (EffectiveType other) const
     {
         return (line != other.line) ? line < other.line
                                     : column <= other.column;
     }
 
+    [[gnu::always_inline]]
     constexpr bool operator> (EffectiveType other) const
     {
         return (line != other.line) ? line > other.line
                                     : column > other.column;
     }
 
+    [[gnu::always_inline]]
     constexpr bool operator>= (EffectiveType other) const
     {
         return (line != other.line) ? line > other.line
                                     : column >= other.column;
     }
 
+    [[gnu::always_inline]]
     constexpr bool operator== (EffectiveType other) const
     {
         return line == other.line and column == other.column;
     }
 
+    [[gnu::always_inline]]
     constexpr bool operator!= (EffectiveType other) const
     {
         return line != other.line or column != other.column;
@@ -76,12 +87,14 @@ struct LineAndColumn
 
 struct ByteCoord : LineAndColumn<ByteCoord, LineCount, ByteCount>
 {
+    [[gnu::always_inline]]
     constexpr ByteCoord(LineCount line = 0, ByteCount column = 0)
         : LineAndColumn(line, column) {}
 };
 
 struct CharCoord : LineAndColumn<CharCoord, LineCount, CharCount>
 {
+    [[gnu::always_inline]]
     constexpr CharCoord(LineCount line = 0, CharCount column = 0)
         : LineAndColumn(line, column) {}
 };
