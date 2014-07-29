9e12f878d868ed195c0d19141d79fa2ce43593ef
davidaciko <davidaciko@gmail.com>
2014-07-29 12:26:35 +0000

    Define macros of all valid locations if more than one

--- src/.find_wide_ncurses.sh
+++ src/.find_wide_ncurses.sh
@@ -1,26 +1,28 @@
 #!/bin/sh
 
-# Finds wide ncurses and defines a macro:
+# Determines relative include path of wide ncurses and defines a macro(s) to 1:
 #   FOUND_NCURSESW_NCURSES_H
 #   FOUND_NCURSESW_H
 #   FOUND_NCURSES_H
 
 
+printf '%s' ' '
+
 printf '%s\n%s\n\0' '#include <ncursesw/ncurses.h>' \
        'int main() { return 0; }' | ${CXX} -xc++ -o /dev/null - 2>/dev/null
 if test "$?" -eq 0 ; then
-  printf ' %s \0' '-DFOUND_NCURSESW_NCURSES_H=1'
-else
-  printf '%s\n%s\n\0' '#include <ncursesw.h>' \
-         'int main() { return 0; }' | ${CXX} -xc++ -o /dev/null - 2>/dev/null
-  if test "$?" -eq 0 ; then
-    printf ' %s \0' '-DFOUND_NCURSESW_H=1'
-  else
-    printf '%s\n%s\n\0' '#include <ncurses.h>' \
-           'int main() { return 0; }' | ${CXX} -xc++ -o /dev/null - 2>/dev/null
-    if test "$?" -eq 0 ; then
-      printf ' %s \0' '-DFOUND_NCURSES_H=1'
-    fi
-  fi
+  printf '%s ' '-DFOUND_NCURSESW_NCURSES_H=1'
+fi
+
+printf '%s\n%s\n\0' '#include <ncursesw.h>' \
+       'int main() { return 0; }' | ${CXX} -xc++ -o /dev/null - 2>/dev/null
+if test "$?" -eq 0 ; then
+  printf '%s ' '-DFOUND_NCURSESW_H=1'
+fi
+
+printf '%s\n%s\n\0' '#include <ncurses.h>' \
+       'int main() { return 0; }' | ${CXX} -xc++ -o /dev/null - 2>/dev/null
+if test "$?" -eq 0 ; then
+  printf '%s ' '-DFOUND_NCURSES_H=1'
 fi
 
