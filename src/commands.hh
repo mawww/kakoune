12e0f46e256866b6771c388ff9aa0b097c86efd5
Maxime Coste <frrrwww@gmail.com>
2015-07-28 20:18:18 +0100

    Use forward declaration instead of include in commands.hh

--- src/commands.hh
+++ src/commands.hh
@@ -1,13 +1,13 @@
 #ifndef commands_hh_INCLUDED
 #define commands_hh_INCLUDED
 
-#include "keys.hh"
 #include "array_view.hh"
 
 namespace Kakoune
 {
 
 class Context;
+class Key;
 
 void register_commands();
 void exec_keys(ConstArrayView<Key> keys, Context& context);
