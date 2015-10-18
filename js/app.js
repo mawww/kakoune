46cf2fa83ff4a763a62203a1a4789243344ab8c8
Maxime Coste <frrrwww@gmail.com>
2015-10-18 15:31:25 +0100

    Improve carousel sliding behaviour, rewind on slide begin, play on slide end

--- js/app.js
+++ js/app.js
@@ -4,11 +4,17 @@
     var cs_demos = $("#carousel-demos");
 
     if (cs_demos) {
-        cs_demos.on("slid.bs.carousel", function (e) {
+        cs_demos.on("slide.bs.carousel", function (e) {
             var video = $(e.relatedTarget).find("video");
-
             if (video) {
                 video[0].currentTime = 0;
+                video[0].pause();
+            }
+        });
+        cs_demos.on("slid.bs.carousel", function (e) {
+            var video = $(e.relatedTarget).find("video");
+            if (video) {
+                video[0].play();
             }
         });
     }
