29bc703d47699214e83bfc8408c41883496ff8a9
Frank LENORMAND <lenormf@gmail.com>
2016-03-23 18:31:25 +0200

    Build kak with libicu, for unicode support with regular expressions

--- src/regex.hh
+++ src/regex.hh
@@ -7,7 +7,7 @@
 #ifdef KAK_USE_STDREGEX
 #include <regex>
 #else
-#include <boost/regex.hpp>
+#include <boost/regex/icu.hpp>
 #endif
 
 namespace Kakoune
@@ -45,27 +45,49 @@ private:
     String m_str;
 };
 namespace regex_ns = std;
+
+template<typename Iterator>
+using RegexIterator = regex_ns::regex_iterator<Iterator>;
+
+template <typename... Args>
+auto regex_match(Args&&... args) -> decltype(std::regex_match(std::forward<Args>(args)...)) {
+  return std::regex_match(std::forward<Args>(args)...);
+}
+template <typename... Args>
+auto regex_search(Args&&... args) -> decltype(std::regex_search(std::forward<Args>(args)...)) {
+  return std::regex_search(std::forward<Args>(args)...);
+}
 #else
-struct Regex : boost::regex
+
+struct Regex : boost::u32regex
 {
     Regex() = default;
 
     explicit Regex(StringView re, flag_type flags = ECMAScript) try
-        : boost::regex(re.begin(), re.end(), flags) {}
+        : boost::u32regex(boost::make_u32regex(re.begin(), re.end(), flags)) {}
         catch (std::runtime_error& err) { throw regex_error(err.what()); }
 
     template<typename Iterator>
     Regex(Iterator begin, Iterator end, flag_type flags = ECMAScript) try
-        : boost::regex(begin, end, flags) {}
+        : boost::u32regex(boost::make_u32regex(begin, end, flags)) {}
         catch (std::runtime_error& err) { throw regex_error(err.what()); }
 
-    String str() const { auto s = boost::regex::str(); return {s.data(), (int)s.length()}; }
+    String str() const { String res; for (auto cp : boost::u32regex::str()) utf8::dump(std::back_inserter(res), cp); return res; }
 };
 namespace regex_ns = boost;
-#endif
 
 template<typename Iterator>
-using RegexIterator = regex_ns::regex_iterator<Iterator>;
+using RegexIterator = regex_ns::u32regex_iterator<Iterator>;
+
+template <typename... Args>
+auto regex_match(Args&&... args) -> decltype(boost::u32regex_match(std::forward<Args>(args)...)) {
+  return boost::u32regex_match(std::forward<Args>(args)...);
+}
+template <typename... Args>
+auto regex_search(Args&&... args) -> decltype(boost::u32regex_search(std::forward<Args>(args)...)) {
+  return boost::u32regex_search(std::forward<Args>(args)...);
+}
+#endif
 
 template<typename Iterator>
 using MatchResults = regex_ns::match_results<Iterator>;
