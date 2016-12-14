430ff37bd63ebb00302ebede37a98095f19983b5
Maxime Coste <mawww@kakoune.org>
2016-12-14 23:50:29 +0000

    Fix regex.hh compilation
    Repeat after me: I will not blindly push commits that I havent
    compiled.
    
    Fixes #990

--- src/regex.hh
+++ src/regex.hh
@@ -20,7 +20,7 @@ struct regex_error : runtime_error
 using RegexBase = boost::basic_regex<wchar_t, boost::c_regex_traits<wchar_t>>;
 
 // Regex that keeps track of its string representation
-class Regex : RegexBase
+class Regex : public RegexBase
 {
 public:
     Regex() = default;
