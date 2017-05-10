3f42e45ff6c758cb6fcca476fbcc4763b2c90305
Olivier Brewaeys <olbrew@users.noreply.github.com>
2017-05-10 12:17:11 +0200

    Small spelling error fix
    John Doe Copyright Waiver
    
      I dedicate any and all copyright interest in this software to the
      public domain.  I make this dedication for the benefit of the public at
      large and to the detriment of my heirs and successors.  I intend this
      dedication to be an overt act of relinquishment in perpetuity of all
      present and future rights to this software under copyright law.

--- src/commands.cc
+++ src/commands.cc
@@ -281,7 +281,7 @@ void edit(const ParametersParser& parser, Context& context, const ShellContext&)
 ParameterDesc edit_params{
     { { "existing", { false, "fail if the file does not exists, do not open a new file" } },
       { "scratch",  { false, "create a scratch buffer, not linked to a file" } },
-      { "debug",    { false, "treate buffer as debug output" } },
+      { "debug",    { false, "create buffer as debug output" } },
       { "fifo",     { true,  "create a buffer reading its content from a named fifo" } },
       { "scroll",   { false, "place the initial cursor so that the fifo will scroll to show new data" } } },
       ParameterDesc::Flags::None, 0, 3
