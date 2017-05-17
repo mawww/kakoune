a578c51bafe1839ccfb3bdc2f50c92bd167bc10c
Maxime Coste <mawww@kakoune.org>
2017-05-17 20:20:31 +0100

    Move constexpr compatible Array struct to meta.hh

--- src/enum.hh
+++ src/enum.hh
@@ -6,17 +6,6 @@
 namespace Kakoune
 {
 
-template<typename T, size_t N>
-struct Array
-{
-    constexpr size_t size() const { return N; }
-    constexpr const T& operator[](int i) const { return m_data[i]; }
-    constexpr const T* begin() const { return m_data; }
-    constexpr const T* end() const { return m_data+N; }
-
-    T m_data[N];
-};
-
 template<typename T> struct EnumDesc { T value; StringView name; };
 
 }
--- src/meta.hh
+++ src/meta.hh
@@ -9,6 +9,18 @@ inline namespace Meta
 template<typename T> struct Type {};
 
 }
+
+template<typename T, size_t N>
+struct Array
+{
+    constexpr size_t size() const { return N; }
+    constexpr const T& operator[](int i) const { return m_data[i]; }
+    constexpr const T* begin() const { return m_data; }
+    constexpr const T* end() const { return m_data+N; }
+
+    T m_data[N];
+};
+
 }
 
 #endif // meta_hh_INCLUDED
