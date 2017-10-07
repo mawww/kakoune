52ee62172a8573352dd62121c70de81117db782a
Maxime Coste <mawww@kakoune.org>
2017-10-07 10:43:21 +0800

    Regex: remove use of buffer_utils.hh from regex_impl.cc

--- src/regex.cc
+++ src/regex.cc
@@ -11,8 +11,14 @@ using Utf8It = RegexUtf8It<const char*>;
 Regex::Regex(StringView re, flag_type flags) try
     : RegexBase{Utf8It{re.begin(), re}, Utf8It{re.end(), re}, flags}, m_str{re.str()}
 {
-    if (auto compiled_regex = compile_regex(re))
-        m_impl = new CompiledRegex{std::move(compiled_regex)};
+    try
+    {
+        m_impl = new CompiledRegex{compile_regex(re)};
+    }
+    catch (runtime_error& err)
+    {
+        write_to_debug_buffer(err.what());
+    }
 } catch (std::runtime_error& err) { throw regex_error(err.what()); }
 
 String option_to_string(const Regex& re)
