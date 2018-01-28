5a1cd1487e441c079881bc817e98da12e0e21250
Alyssa Ross <hi@alyssa.is>
2018-01-28 19:48:43 +0000

    Various Homebrew formula improvements
    * Add missing dependencies on Linux
    * Remove unnecessary option. Hasn't been necessary since
      Homebrew/homebrew-core@90aae3e04c9af9d834c16c6820aff930fed5c3f4.
    * `require "formula"` is unnecessary
    * `brew audit --strict` warns on single quotes

--- contrib/kakoune.rb
+++ contrib/kakoune.rb
@@ -1,17 +1,20 @@
-require "formula"
-
 class Kakoune < Formula
   homepage "https://github.com/mawww/kakoune"
   head "https://github.com/mawww/kakoune.git"
 
-  depends_on 'docbook-xsl' => :build
-  depends_on 'ncurses' => [:build, :recommended]
-  depends_on 'asciidoc' => [:build, 'with-docbook-xsl']
+  depends_on "docbook-xsl" => :build
+  depends_on "ncurses" => [:build, :recommended]
+  depends_on "asciidoc" => :build
+
+  unless OS.mac?
+    depends_on "libxslt" => :build
+    depends_on "pkg-config" => :build
+  end
 
   def install
     ENV["XML_CATALOG_FILES"] = "#{etc}/xml/catalog"
 
-    cd 'src' do
+    cd "src" do
       system "make", "install", "PREFIX=#{prefix}", "debug=no"
     end
   end
