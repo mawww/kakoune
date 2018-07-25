a6383cd9f31ad29bf6758e0231f6d1534995815f
Yuxuan Shui <yshuiv7@gmail.com>
2018-07-25 15:04:20 +0100

    Interpret the ncurses_set_title options as the title line
    When ncurses_set_title is set to a string other than "yes" and
    "true", it will be used to set the title line. This give the user
    the ability to configure the title line.

--- src/ncurses_ui.hh
+++ src/ncurses_ui.hh
@@ -145,6 +145,7 @@ private:
     int m_shift_function_key = default_shift_function_key;
 
     bool m_set_title = true;
+    String m_title_line;
     bool m_change_colors = true;
 
     bool m_dirty = false;
