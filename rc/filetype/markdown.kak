mawww/kakoune

commit 89b50daa66cc843e256f599462bb57bce694d923

Use module alias pattern for markdown dynamic loading

rc/filetype/markdown.kak
@@ -16,12 +16,13 @@ hook global WinSetOption filetype=markdown %{
 
     hook window InsertChar \n -group markdown-indent markdown-indent-on-new-line
     hook -once -always window WinSetOption filetype=.* %{ remove-hooks window markdown-.+ }
-
-    hook -group markdown-load buffer NormalIdle .* %{ try %{ evaluate-commands -draft %{
-        execute-keys '%s^\h*```\h*\K[^\n]+$<ret>'
-        evaluate-commands -itersel %{ require-module %val{selection} }
-    }}}
 }
+
+hook -group markdown-load-languages global WinSetOption filetype=markdown %{
+    hook window NormalIdle .* markdown-load-languages
+    hook window InsertIdle .* markdown-load-languages
+}
+
 
 hook -group markdown-highlight global WinSetOption filetype=markdown %{
     add-highlighter window/markdown ref markdown
@@ -92,6 +93,13 @@ define-command -hidden markdown-indent-on-new-line %{
         # remove trailing white spaces
         try %{ execute-keys -draft -itersel %{ k<a-x> s \h+$ <ret> d } }
     }
+}
+
+define-command -hidden markdown-load-languages %{
+    evaluate-commands -draft %{ try %{
+        execute-keys 'gtGbGls```\h*\K[^\s]+<ret>'
+        evaluate-commands -itersel %{ require-module %val{selection} }
+    }}
 }
 
 }

rc/filetype/c-family.kak
@@ -1,3 +1,4 @@
+# Detection
 hook global BufCreate .*\.(cc|cpp|cxx|C|hh|hpp|hxx|H)$ %{
     set-option buffer filetype cpp
 }
@@ -57,6 +58,11 @@ hook -group objc-highlight global WinSetOption filetype=objc %{
     add-highlighter window/objc ref objc
     hook -once -always window WinSetOption filetype=.* %{ remove-highlighter window/objc }
 }
+
+# Module aliases
+provide-module c %{ require-module c-family }
+provide-module cpp %{ require-module c-family }
+provide-module objc %{ require-module c-family }
 
 
 provide-module c-family %§

rc/filetype/javascript.kak
@@ -34,6 +34,9 @@ hook -group typescript-highlight global WinSetOption filetype=typescript %{
     hook -once -always window WinSetOption filetype=.* %{ remove-highlighter window/typescript }
 }
 
+# Aliases
+# ‾‾‾‾‾‾‾
+provide-module typescript %{ require-module javascript }
 
 provide-module javascript %§
 

commit 4d7bef028e69d48ee1a968d2f96f3394881be764

fix errors on markdown files introduced by 89b50daa
89b50daa introduced a bug whereby opening a markdown file results
in the following error:

    error running hook NormalIdle()/markdown-load-languages: no such command: 'markdown-load-languages'

I'm not an expert in kakoune scripting, but I think the problem is
that `markdown-load-languages` is in its own module, while the hook
command is not.

rc/filetype/markdown.kak
@@ -18,6 +18,13 @@ hook global WinSetOption filetype=markdown %{
     hook -once -always window WinSetOption filetype=.* %{ remove-hooks window markdown-.+ }
 }
 
+define-command -hidden markdown-load-languages %{
+    evaluate-commands -draft %{ try %{
+        execute-keys 'gtGbGls```\h*\K[^\s]+<ret>'
+        evaluate-commands -itersel %{ require-module %val{selection} }
+    }}
+}
+
 hook -group markdown-load-languages global WinSetOption filetype=markdown %{
     hook -group markdown-load-languages window NormalIdle .* markdown-load-languages
     hook -group markdown-load-languages window InsertIdle .* markdown-load-languages
@@ -93,13 +100,6 @@ define-command -hidden markdown-indent-on-new-line %{
         # remove trailing white spaces
         try %{ execute-keys -draft -itersel %{ k<a-x> s \h+$ <ret> d } }
     }
-}
-
-define-command -hidden markdown-load-languages %{
-    evaluate-commands -draft %{ try %{
-        execute-keys 'gtGbGls```\h*\K[^\s]+<ret>'
-        evaluate-commands -itersel %{ require-module %val{selection} }
-    }}
 }
 
 }