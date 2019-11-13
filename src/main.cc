c12699d2e9c2806d6ed184032078d0b84a3370bb
Maxime Coste <mawww@kakoune.org>
2019-11-13 23:31:24 +1100

    Replace ncurses with a naive custom terminal backend

--- src/main.cc
+++ src/main.cc
@@ -1000,7 +1000,7 @@ int run_pipe(StringView session)
 
 void signal_handler(int signal)
 {
-    NCursesUI::abort();
+    NCursesUI::restore_terminal();
     const char* text = nullptr;
     switch (signal)
     {
