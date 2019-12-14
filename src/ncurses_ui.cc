e7e728981359adbbd74ccf4dfc0053db0d77cd41
fsub <31548809+fsub@users.noreply.github.com>
2019-12-14 23:17:34 +0100

    Support rxvt style s-F3 to s-F10

--- src/ncurses_ui.cc
+++ src/ncurses_ui.cc
@@ -712,6 +712,14 @@ Optional<Key> NCursesUI::get_next_key()
                 return masked_key(Key::F6 + params[0] - 17);
             case 23: case 24:
                 return masked_key(Key::F11 + params[0] - 23);
+            case 25: case 26:
+                return Key{Key::Modifiers::Shift, Key::F3 + params[0] - 25}; // rxvt style
+            case 28: case 29:
+                return Key{Key::Modifiers::Shift, Key::F5 + params[0] - 28}; // rxvt style
+            case 31: case 32:
+                return Key{Key::Modifiers::Shift, Key::F7 + params[0] - 31}; // rxvt style
+            case 33: case 34:
+                return Key{Key::Modifiers::Shift, Key::F9 + params[0] - 33}; // rxvt style
             }
             return {};
         case 'u':
