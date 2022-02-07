mawww/kakoune

commit 6f7c5aed107c183565661336ae840998a73e783f

Do not show custom completions when autocomplete is off
As reported in [1], completions provided by "set global completers
option=my_completion" activate insert mode autocompletion, even when
the autocomplete option does not have the insert mode flag.

This happens because InsertCompleter::on_option_changed() calls
InsertCompleter::setup_ifn(), which shows the completion pager.
Fix this by computing whether the completion pager is enabled;
otherwise we can return early from setup_ifn().
The completion pager is enabled if the autocompletion bit is set,
or if the user has requested explicit completion.

[1]: https://github.com/kak-lsp/kak-lsp/issues/585

src/insert_completer.cc
@@ -461,12 +461,12 @@ void InsertCompleter::select(int index, bool relative, Vector<Key>& keystrokes)
 
 void InsertCompleter::update(bool allow_implicit)
 {
+    m_enabled = allow_implicit or m_explicit_completer;
     if (m_explicit_completer and try_complete(m_explicit_completer))
         return;
 
     reset();
-    if (allow_implicit)
-        setup_ifn();
+    setup_ifn();
 }
 
 auto& get_first(BufferRange& range) { return range.begin; }
@@ -500,6 +500,8 @@ void InsertCompleter::reset()
 
 bool InsertCompleter::setup_ifn()
 {
+    if (!m_enabled)
+        return false;
     using namespace std::placeholders;
     if (not m_completions.is_valid())
     {

test/regression/0-autocomplete-overrules-completers/in
@@ -1,0 +1,3 @@
+
+
+

test/regression/0-autocomplete-overrules-completers/out
@@ -1,0 +1,3 @@
+
+a2
+./ui-in

test/regression/0-autocomplete-overrules-completers/rc
@@ -1,0 +1,6 @@
+set-option global autocomplete prompt
+declare-option -hidden completions line1_completions
+declare-option -hidden completions line2_completions
+set-option global completers option=line1_completions option=line2_completions
+set-option global line1_completions "1.1+0@%val(timestamp)" "a1||a1"
+set-option global line2_completions "2.1+0@%val(timestamp)" "a2||a2"

test/regression/0-autocomplete-overrules-completers/script
@@ -1,0 +1,10 @@
+ui_out -ignore 4
+ui_in '{ "jsonrpc": "2.0", "method": "keys", "params": [ "i" ] }'
+sleep .2 # trigger insert completion auto update
+# Implicit completion is disabled via autocomplete.
+ui_in '{ "jsonrpc": "2.0", "method": "keys", "params": [ "<c-n><esc>" ] }'
+# Implicit completion can be toggled.
+ui_in '{ "jsonrpc": "2.0", "method": "keys", "params": [ "ji<c-o><c-n><esc>" ] }'
+# Explicit completion still works.
+ui_in '{ "jsonrpc": "2.0", "method": "keys", "params": [ "ji./ui-<c-x>f<c-n>" ] }'
+

src/insert_completer.hh
@@ -107,6 +107,7 @@ private:
     InsertCompletion    m_completions;
     Vector<BufferRange> m_inserted_ranges;
     int                 m_current_candidate = -1;
+    bool                m_enabled = true;
 
     using CompleteFunc = InsertCompletion (const SelectionList& sels,
                                            const OptionManager& options,

commit 43fc5b0078db48583f1222d8b9b96ba87f2e8fc7

Make <c-n> show completion menu again when autocomplete is off
As pointed out in [1], when insert mode autocomplete is disabled,
<c-n> could be used to activate insert mode completions temporarily
[2].  This regressed in 6f7c5aed (Do not show custom completions when
autocomplete is off, 2022-01-03). Fix this by enabling completions
on <c-n>/<c-p>. This allows us to remove a special case for explicit
completers.

Alternative behavior (future?): make <c-n> toggle completion like
<c-o>.  This can be done today, as suggested by Screwtape on IRC:

	map global insert <c-n> %{<c-o><c-n><a-;>:toggle-ctrl-n<ret>}
	define-command toggle-ctrl-n %{
		hook global InsertCompletionShow .* %{ map window insert <c-n> <c-n> }
		hook global InsertCompletionHide .* %{ unmap window insert <c-n> <c-n> }
	}

[1] https://github.com/mawww/kakoune/pull/4493#issuecomment-1031189823
[2] <c-n> completion only lives for the lifetime of the completion
    menu, whereas <c-o> lasts until you exit insert mode. This means
    that autocompletion is much more convenient than <c-n> or <c-x>f,
    because those require an explicit completion request for each
    path component.

src/insert_completer.cc
@@ -414,6 +414,7 @@ InsertCompleter::~InsertCompleter()
 
 void InsertCompleter::select(int index, bool relative, Vector<Key>& keystrokes)
 {
+    m_enabled = true;
     if (not setup_ifn())
         return;
 
@@ -461,7 +462,7 @@ void InsertCompleter::select(int index, bool relative, Vector<Key>& keystrokes)
 
 void InsertCompleter::update(bool allow_implicit)
 {
-    m_enabled = allow_implicit or m_explicit_completer;
+    m_enabled = allow_implicit;
     if (m_explicit_completer and try_complete(m_explicit_completer))
         return;
 

test/regression/0-autocomplete-overrules-completers/in
@@ -1,3 +1,4 @@
 
 
 
+

test/regression/0-autocomplete-overrules-completers/out
@@ -1,3 +1,4 @@
 
 a2
+a3
 ./ui-in

test/regression/0-autocomplete-overrules-completers/rc
@@ -1,6 +1,8 @@
 set-option global autocomplete prompt
 declare-option -hidden completions line1_completions
 declare-option -hidden completions line2_completions
-set-option global completers option=line1_completions option=line2_completions
+declare-option -hidden completions line3_completions
+set-option global completers option=line1_completions option=line2_completions option=line3_completions
 set-option global line1_completions "1.1+0@%val(timestamp)" "a1||a1"
 set-option global line2_completions "2.1+0@%val(timestamp)" "a2||a2"
+set-option global line3_completions "3.1+0@%sh{echo $(($kak_timestamp+1))}" "a3||a3"

test/regression/0-autocomplete-overrules-completers/script
@@ -1,10 +1,10 @@
 ui_out -ignore 4
 ui_in '{ "jsonrpc": "2.0", "method": "keys", "params": [ "i" ] }'
 sleep .2 # trigger insert completion auto update
-# Implicit completion is disabled via autocomplete.
-ui_in '{ "jsonrpc": "2.0", "method": "keys", "params": [ "<c-n><esc>" ] }'
-# Implicit completion can be toggled.
-ui_in '{ "jsonrpc": "2.0", "method": "keys", "params": [ "ji<c-o><c-n><esc>" ] }'
+# Implicit completion can be toggled with <c-o>.
+ui_in '{ "jsonrpc": "2.0", "method": "keys", "params": [ "<esc>ji<c-o><c-n><esc>" ] }'
+# Implicit completion can be toggled with <c-n>.
+ui_in '{ "jsonrpc": "2.0", "method": "keys", "params": [ "ji<c-n><esc>" ] }'
 # Explicit completion still works.
 ui_in '{ "jsonrpc": "2.0", "method": "keys", "params": [ "ji./ui-<c-x>f<c-n>" ] }'
 