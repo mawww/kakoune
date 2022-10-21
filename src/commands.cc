2277427a01791feadf354e011f7abeb75b018659
Johannes Altmanninger <aclopte@gmail.com>
2022-10-21 21:50:34 +0200

    Allow "define-command -menu" only if a completer is passed as well
    The "define-command -menu" flag does not do anything unless there is
    a completer.  Let's forbid it so there is no confusion about whether
    this can be mixed with "complete-command".

--- src/commands.cc
+++ src/commands.cc
@@ -1286,7 +1286,8 @@ void define_command(const ParametersParser& parser, Context& context, const Shel
     if (parser.get_switch("hidden"))
         flags = CommandFlags::Hidden;
 
-    const Completions::Flags completions_flags = parser.get_switch("menu") ?
+    bool menu = (bool)parser.get_switch("menu");
+    const Completions::Flags completions_flags = menu ?
         Completions::Flags::Menu : Completions::Flags::None;
 
     const String& commands = parser[1];
@@ -1323,6 +1324,8 @@ void define_command(const ParametersParser& parser, Context& context, const Shel
     }
 
     CommandCompleter completer = parse_completion_switch(parser, completions_flags);
+    if (menu and not completer) 
+        throw runtime_error(format("menu switch requires a completion switch", cmd_name));
     auto docstring = trim_indent(parser.get_switch("docstring").value_or(StringView{}));
 
     cm.register_command(cmd_name, cmd, docstring, desc, flags, CommandHelper{}, std::move(completer));
