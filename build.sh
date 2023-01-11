a9b9e824e993a2b78d2b04abc3828760999118d1
loovjo <jonathan.loov@gmail.com>
2023-01-11 20:32:01 +0100

    Update build

--- build.sh
+++ build.sh
@@ -3,5 +3,7 @@ PATH="${gnumake}/bin:${clang}/bin:/usr/bin:/bin"
 cp -r $src/* .
 chmod -R 774 .
 cd src
-make -j $NIX_BUILD_CORES
+# TODO: it seems most nix projects use -j $NIX_BUILD_CORES
+# However by default it is 0, which makes make die
+make -j
 PREFIX=$out make install
