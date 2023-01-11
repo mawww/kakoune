efcdb3b8b53728db714bf98e88087b95969604e4
loovjo <jonathan.loov@gmail.com>
2023-01-11 22:37:29 +0100

    unnixify

--- build.sh
+++
@@ -1,9 +0,0 @@
-PATH="${gnumake}/bin:${clang}/bin:/usr/bin:/bin"
-
-cp -r $src/* .
-chmod -R 774 .
-cd src
-# TODO: it seems most nix projects use -j $NIX_BUILD_CORES
-# However by default it is 0, which makes make die
-make -j
-PREFIX=$out make install
