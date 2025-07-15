878ee871dd3efadc797f6d0226755d7911e6e866
Adrià Arrufat <adria.arrufat@gmail.com>
2025-07-15 22:59:29 +0900

    feat(client): allow 'on'/'off' for autoreload option

--- src/client.hh
+++ src/client.hh
@@ -162,7 +162,9 @@ constexpr auto enum_desc(Meta::Type<Autoreload>)
         { Autoreload::No, "no" },
         { Autoreload::Ask, "ask" },
         { Autoreload::Yes, "true" },
-        { Autoreload::No, "false" }
+        { Autoreload::No, "false" },
+        { Autoreload::Yes, "on" },
+        { Autoreload::No, "off" }
     });
 }
 
