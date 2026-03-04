mawww/kakoune

commit 551b20a34aeab6e1736da42d9ae5b0356678a7f3

Add a hidden indicator in the normal mode mode line
This should make it more obvious that some selections are not visible,
helping users avoid unintended off-screen modifications of the buffer.

Fixes #5425
Fixes #2727

src/client.cc
@@ -248,16 +248,6 @@ void Client::redraw_ifn()
     if (window.needs_redraw(context()))
         m_ui_pending |= Draw;
 
-    DisplayLine mode_line = generate_mode_line();
-    if (mode_line.atoms() != m_mode_line.atoms())
-    {
-        m_ui_pending |= StatusLine;
-        m_mode_line = std::move(mode_line);
-    }
-
-    if (m_ui_pending == 0)
-        return;
-
     const auto& faces = context().faces();
 
     if (m_ui_pending & Draw)
@@ -305,6 +295,14 @@ void Client::redraw_ifn()
     if (m_ui_pending & InfoHide)
         m_ui->info_hide();
 
+    // This needs to be done *after* update_display_buffer as ithe mode line may rely on it to
+    // compute whether selections are visible.
+    DisplayLine mode_line = generate_mode_line();
+    if (mode_line.atoms() != m_mode_line.atoms())
+    {
+        m_ui_pending |= StatusLine;
+        m_mode_line = std::move(mode_line);
+    }
     if (m_ui_pending & StatusLine)
         m_ui->draw_status(m_status_prompt, m_status_content, m_status_cursor_pos, m_mode_line, faces["StatusLine"]);
 

src/input_handler.cc
@@ -381,23 +381,37 @@ public:
 
     ModeInfo mode_info() const override
     {
+        auto info_face = context().faces()["StatusLineInfo"];
+        auto value_face = context().faces()["StatusLineValue"];
+
+        auto& sels = context().selections();
+        int hidden_count = 0;
+        for (auto& sel : sels)
+            hidden_count += context().has_window() and not context().window().display_coord(sel.cursor());
+
         AtomList atoms;
-        auto num_sel = context().selections().size();
-        auto main_index = context().selections().main_index();
-        if (num_sel == 1)
-            atoms.emplace_back(format("{} sel", num_sel), context().faces()["StatusLineInfo"]);
+        if (sels.size() == 1)
+        {
+            atoms.emplace_back(format("{} sel", sels.size()), info_face);
+            if (hidden_count)
+                atoms.emplace_back(" (hidden)", info_face);
+        }
         else
-            atoms.emplace_back(format("{} sels ({})", num_sel, main_index + 1), context().faces()["StatusLineInfo"]);
+        {
+            atoms.emplace_back(format("{} sels ({})", sels.size(), sels.main_index() + 1), info_face);
+            if (hidden_count)
+                atoms.emplace_back(format(" ({} hidden)", hidden_count), info_face);
+        }
 
         if (m_params.count != 0)
         {
-            atoms.emplace_back(" param=", context().faces()["StatusLineInfo"]);
-            atoms.emplace_back(to_string(m_params.count), context().faces()["StatusLineValue"]);
+            atoms.emplace_back(" param=", info_face);
+            atoms.emplace_back(to_string(m_params.count), value_face);
         }
         if (m_params.reg)
         {
-            atoms.emplace_back(" reg=", context().faces()["StatusLineInfo"]);
-            atoms.emplace_back(StringView(m_params.reg).str(), context().faces()["StatusLineValue"]);
+            atoms.emplace_back(" reg=", info_face);
+            atoms.emplace_back(StringView(m_params.reg).str(), value_face);
         }
         return {atoms, m_params};
     }

commit 03019caf858888cac0e30e92b357416c0113e7d8

fix(client): refresh only if m_ui_pending is not empty
Fixes regression introduced in 551b20a where moving `generate_mode_line`
after `update_display_buffer` removed the early return check. This caused
`refresh` to be called even when no UI updates were needed, breaking tests
that expected specific message sequences.

Fixes: 551b20a34aeab6e1736da42d9ae5b0356678a7f3
Signed-Off-By: Paal Øye-Strømme <paal.o.eye@gmail.com>

src/client.cc
@@ -306,7 +306,9 @@ void Client::redraw_ifn()
     if (m_ui_pending & StatusLine)
         m_ui->draw_status(m_status_prompt, m_status_content, m_status_cursor_pos, m_mode_line, faces["StatusLine"]);
 
-    m_ui->refresh(m_ui_pending & Refresh);
+    if (m_ui_pending != 0)
+        m_ui->refresh(m_ui_pending & Refresh);
+
     m_ui_pending = 0;
 }
 